f24fccc66dd0497c1d6f205bf43ed980af979780
Ignatius22 <igee54077@gmail.com>
2020-07-04 22:57:23 +0100

    add image link

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -8,6 +8,7 @@ class Landing extends Component {
                 <div className="container">
                     <div className="jumbotron mt-5">
                         <h1 className="col-sm-8 mx-auto display-4">Soul Food</h1>
+                        <img src="https://cdn.pixabay.com/photo/2014/02/13/07/28/wordpress-265132_960_720.jpg" alt="landing" />
                     </div>
                    
                 </div>
